5342bb562af219c1c512617fc2a79ba49980f51e
Novandri Sitinjak <novandristnjk@gmail.com>
2025-11-24 19:18:05 +0700

    add middleware

--- source/main.py
+++ source/main.py
@@ -8,8 +8,22 @@ import traceback
 from dto.chat import ChatDTO
 from dto.upload import UploadDTO
 from util.extractor import extract_text_by_page
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 INDEX_DIR = "index_data"
 os.makedirs(INDEX_DIR, exist_ok=True)
@@ -51,7 +65,7 @@ if __name__ == "__main__":
         formatter_class=argparse.RawDescriptionHelpFormatter
     ) 
 
-    argument_parser.add_argument('-p', '--port', help='Port', metavar='', default=8027, type=int)
+    argument_parser.add_argument('-p', '--port', help='Port', metavar='', default=8000, type=int)
     argument_parser.add_argument('-w', '--worker', help='Worker', metavar='', default=1, type=int)
     args = argument_parser.parse_args()
 
