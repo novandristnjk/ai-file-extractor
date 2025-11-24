9873ab7a867d3a5770614186b454800e5683fa89
Novandri Sitinjak <novandristnjk@gmail.com>
2025-11-24 21:04:04 +0700

    add git ignore

--- source/main.py
+++ source/main.py
@@ -19,6 +19,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        # "http://localhost:5173",
+        "http://localhost:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
